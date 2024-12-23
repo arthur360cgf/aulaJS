aff004487f8a1fe5fea8f5649260901866cb11cb
Arthur360cgf <arthurmattei115@gmail.com>
2024-12-23 00:48:22 -0300

    classList

--- classList/script/script.js
+++ classList/script/script.js
@@ -4,6 +4,21 @@
  */
 const menu = document.querySelector('.menu');
 
+console.log(menu.classList);
+menu.classList.add('ativo', 'teste');
+menu.classList.remove('azul');
+menu.classList.toggle('azul');
+menu.classList.toggle('azul');
+ 
+if(menu.classList.contains('azul')){
+    menu.classList.add('possui-azul');
+}else{
+    menu.classList.add('nao-possui-azul');
+}
+//concatenar string
+menu.className = menu.className + 'vermenlho';
+menu.className += 'azul';
+/** 
 menu.className;// string
 menu.classList;// lista de classes
 menu.classList.add('ativo');
@@ -12,3 +27,10 @@ menu.classList.remove('ativo');
 menu.classList.toggle('ativo');// adiciona/remove uma classe
 menu.classList.contains('ativo');// true ou false
 menu.classList.replace('ativo', 'inativo');
+*/
+/**
+ * retorna um array-like com os atributos do elemento 
+ */
+const animais = document.querySelector('.animais');
+console.log(animais.attributes);
+console.log(animais.attributes[0]);
